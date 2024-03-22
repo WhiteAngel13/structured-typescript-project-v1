62b47ebc69d4d073bd4af8ca86cc7edb55b96118
WhiteAngel13 <edmilsonsw22@gmail.com>
2024-03-22 14:59:11 -0400

    adicionando zod, apresentando milson e tarefa de 'reflect-metadata'

--- src/account/controllers/rest/account.rest.controller.ts
+++ src/account/controllers/rest/account.rest.controller.ts
@@ -1,8 +1,14 @@
 import { BalanceAccountRestServiceResponseDTO } from './services/balance/balance.account.rest.service.dtos';
 import { CreateAccountRestService } from './services/create/create.account.rest.service';
-import { CreateAccountRestServiceResponseDTO } from './services/create/create.account.rest.service.dtos';
+import {
+  CreateAccountRestServiceParamsDTOSchema,
+  CreateAccountRestServiceResponseDTO,
+} from './services/create/create.account.rest.service.dtos';
 import { DepositAccountRestService } from './services/deposit/deposit.account.rest.service';
-import { DepositAccountRestServiceResponseDTO } from './services/deposit/deposit.account.rest.service.dtos';
+import {
+  DepositAccountRestServiceParamsDTOSchema,
+  DepositAccountRestServiceResponseDTO,
+} from './services/deposit/deposit.account.rest.service.dtos';
 import { WithdrawAccountRestService } from './services/withdraw/withdraw.account.rest.service';
 import { WithdrawAccountRestServiceResponseDTO } from './services/withdraw/withdraw.account.rest.service.dtos';
 import { BalanceAccountRestService } from './services/balance/balance.account.rest.service';
@@ -15,32 +21,15 @@ export class AccountRestController {
     private readonly balanceAccountRestService: BalanceAccountRestService,
   ) {}
   async create(params: unknown): Promise<CreateAccountRestServiceResponseDTO> {
-    if (typeof params !== 'object') throw new Error('invalid params');
-    if (params === null) throw new Error('invalid params');
-    if (Array.isArray(params)) throw new Error('invalid params');
-    const { nickname } = params as { nickname?: unknown };
-    if (typeof nickname !== 'string') throw new Error('invalid nickname');
-
-    return this.createAccountRestService.execute({ nickname });
+    const paramsDTO = CreateAccountRestServiceParamsDTOSchema.parse(params);
+    return this.createAccountRestService.execute(paramsDTO);
   }
 
   async deposit(
     params: unknown,
   ): Promise<DepositAccountRestServiceResponseDTO> {
-    if (typeof params !== 'object') throw new Error('invalid params');
-    if (params === null) throw new Error('invalid params');
-    if (Array.isArray(params)) throw new Error('invalid params');
-    const { nickname } = params as { nickname?: unknown };
-    if (typeof nickname !== 'string') throw new Error('invalid nickname');
-    const { value } = params as { value?: unknown };
-    if (typeof value !== 'string') throw new Error('invalid value');
-    const valueNumber = Number(value);
-    if (isNaN(valueNumber)) throw new Error('invalid value');
-
-    return this.depositAccountRestService.execute({
-      nickname,
-      value: valueNumber,
-    });
+    const paramsDTO = DepositAccountRestServiceParamsDTOSchema.parse(params);
+    return this.depositAccountRestService.execute(paramsDTO);
   }
 
   async withdraw(
